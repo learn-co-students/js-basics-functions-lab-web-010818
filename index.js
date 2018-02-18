adffa837288b922fcd5e4b5c66f6bd8cdc8852b7
TorreJohnson <torre.johnson@gmail.com>
2018-02-18 17:45:27 -0500

    completed Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(loc) {
+  if (loc > 42) {
+    return loc - 42;
+  } else {
+    return 42 - loc;
+  }
+}
+
+function distanceFromHqInFeet(loc) {
+  return distanceFromHqInBlocks(loc) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (start > end) {
+    return (start - end) * 264;
+  } else {
+    return (end - start) * 264;
+  }
+}
+
+function calculatesFarePrice(start, dest) {
+  const distance = distanceTravelledInFeet(start, dest);
+  if(distance < 400) {
+    return 0;
+  } else if(distance <= 2000) {
+    return distance * 0.02;
+  } else if(distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
